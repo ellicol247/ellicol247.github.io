d6a3b21c0cea087bf0d3d11e4c3a7a24f5e6a95c
Colson Elliott <joesheisty9@gmail.com>
2025-09-21 22:56:51 -0600

    Update wa5.js

--- wa/wa5/wa5.js
+++ wa/wa5/wa5.js
@@ -2,13 +2,13 @@ const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
 navToggle.addEventListener('click', () => {
-  // Toggle the 'show' class to show/hide the menu
+  
   navMenu.classList.toggle('show');
-   navToggle.classList.toggle('open'); // <-- add this line
+   navToggle.classList.toggle('open'); 
 
-  // Get current state of aria-expanded (true or false)
+  
   const isExpanded = navToggle.getAttribute('aria-expanded') === 'true';
 
-  // Set the opposite of current value
+  
   navToggle.setAttribute('aria-expanded', !isExpanded);
 });
